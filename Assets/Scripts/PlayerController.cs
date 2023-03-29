3138bcff9dce1df52046b83919efa4369e6aea7d
kakorolling <kakorolling@gmail.com>
2023-03-29 13:40:50 +0900

    add animation
    간단한 애니메이션 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    Animator PlayerAnim;
+
     void Update()
     {
 
