614daf5a3b742f981eea3c3256e4663242c1adfc
givebro <plewe97@naver.com>
2023-04-02 00:12:16 +0900

    인벤UI
    인벤 UI

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject inventoyuPanel;
+    bool activelInventory = false;
+    
+    private void Start(){
+        inventoyuPanel.SetActive(activelInventory);
+    }
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.E)){
+                activelInventory = !activelInventory;
+                inventoyuPanel.SetActive(activelInventory);
+        }
+    
+  
+}
+}
