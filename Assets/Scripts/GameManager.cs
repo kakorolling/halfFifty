147579c46f499c535c02bd9cd3d1070a30168b72
kakorolling <kakorolling@gmail.com>
2023-04-07 01:03:30 +0900

    system 업데이트
    업데이트

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,13 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);//씬 전환이 되더라도 파괴되지 않게 한다.
     }
 
+    public void LoadChunk(string chunkData) //청크를 로드하는 거
+    {
+        Vector2Int idx;
+        Chunk chunk = null;
+        loadedMap.Add(idx, chunk);
+    }
+
     public (int terrain, List<GameObject> objList, bool collider) GetTileData(Vector2Int worldTileIdx) //좌표값을 받아서 그 좌표의 타일 정보 3가지를 반환
     {
         Vector2Int chunkIdx = new Vector2Int(worldTileIdx.x / chunkSize.x, worldTileIdx.y / chunkSize.y); //청크 좌표값 구하기
@@ -32,9 +39,20 @@ public class GameManager : MonoBehaviour
         return (terrain, objList, collider);
     }
 
-    public GameObject CreateObj(Vector2Int position)
+    public void LoadObj(Vector2Int position, string objData) //모든 오브젝트에는 데이터가 있어야함. 
     {
+        GameObject newObj = new GameObject();
+        Id id = newObj.AddComponent<Id>();
+        id.value =; //
 
+        MoveObj(newObj, position);
+    }
+
+
+    public bool MoveObj(GameObject obj, Vector2Int position) // 실제 격자공간에 오브젝트를 넣음, 그에 맞게 GameScene의 위치도 바꿈
+    {
+        return new bool();
     }
 
 }
+
