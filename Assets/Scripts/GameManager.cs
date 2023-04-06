b3c25ad1a3ced89eeb8c724e4fbb59f84400386b
kakorolling <kakorolling@gmail.com>
2023-04-07 01:49:18 +0900

    system 약간 수정
    그러합니다

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,26 +18,26 @@ public class GameManager : MonoBehaviour
         //싱글톤 패턴
         if (instance != null) Destroy(gameObject);
         instance = this;//이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
-        DontDestroyOnLoad(gameObject);//씬 전환이 되더라도 파괴되지 않게 한다.
+        DontDestroyOnLoad(gameObject); //씬 전환이 되더라도 파괴되지 않게 한다.
     }
 
     public void LoadChunk(string chunkData) //청크를 로드하는 거
     {
         Vector2Int idx;
         Chunk chunk = null;
-        loadedMap.Add(idx, chunk);
+        loadedMap.Add(idx, chunk); //청크의 인덱스값으로 청크를 불러옴
     }
 
-    public (int terrain, List<GameObject> objList, bool collider) GetTileData(Vector2Int worldTileIdx) //좌표값을 받아서 그 좌표의 타일 정보 3가지를 반환
-    {
-        Vector2Int chunkIdx = new Vector2Int(worldTileIdx.x / chunkSize.x, worldTileIdx.y / chunkSize.y); //청크 좌표값 구하기
-        Chunk chunk = loadedMap[chunkIdx];
-        Vector2Int chunkTileIdx = new Vector2Int(worldTileIdx.x % chunkSize.x, worldTileIdx.y % chunkSize.y); //한 청크 기준 타일 좌표값 구하기
-        int terrain = chunk.terrainLayer[chunkTileIdx.x, chunkTileIdx.y]; //한 청크안의 좌표의 지형 값을 불러오기
-        List<GameObject> objList = chunk.objLayer[chunkTileIdx.x, chunkTileIdx.y]; //한 청크안의 좌표의 오브젝트 리스트를 불러오기
-        bool collider = chunk.colliderLayer[chunkTileIdx.x, chunkTileIdx.y]; //한 청크안의 좌표의 충돌 여부 불러오기
-        return (terrain, objList, collider);
-    }
+    // public (int terrain, List<GameObject> objList, bool collider) GetTileData(Vector2Int worldTileIdx) //좌표값을 받아서 그 좌표의 타일 정보 3가지를 반환
+    // {
+    //     Vector2Int chunkIdx = new Vector2Int(worldTileIdx.x / chunkSize.x, worldTileIdx.y / chunkSize.y); //청크 좌표값 구하기
+    //     Chunk chunk = loadedMap[chunkIdx];
+    //     Vector2Int chunkTileIdx = new Vector2Int(worldTileIdx.x % chunkSize.x, worldTileIdx.y % chunkSize.y); //한 청크 기준 타일 좌표값 구하기
+    //     int terrain = chunk.terrainLayer[chunkTileIdx.x, chunkTileIdx.y]; //한 청크안의 좌표의 지형 값을 불러오기
+    //     List<GameObject> objList = chunk.objLayer[chunkTileIdx.x, chunkTileIdx.y]; //한 청크안의 좌표의 오브젝트 리스트를 불러오기
+    //     bool collider = chunk.colliderLayer[chunkTileIdx.x, chunkTileIdx.y]; //한 청크안의 좌표의 충돌 여부 불러오기
+    //     return (terrain, objList, collider);
+    // }
 
     public void LoadObj(Vector2Int position, string objData) //모든 오브젝트에는 데이터가 있어야함. 
     {
---
+++ Assets/Scripts/Properties/Durability.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Durability : MonoBehaviour
+{
+    public int maxValue;
+    public int value;
+}
+
