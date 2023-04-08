39c675a446850883b0172b1ee415e23df8bd8176
kakorolling <kakorolling@gmail.com>
2023-04-09 03:05:54 +0900

    add image
    ㄷ ㅁ 타일 추가

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -5,34 +5,37 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     [SerializeField] private GameObject Pause_UI;
-   
-    
+
+
     void Start()
     {
-       Pause_UI.SetActive(false);
+        Pause_UI.SetActive(false);
     }
 
-    
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.P)){
-            if(!GameManager.isPause)
-                CallMenu();
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!GameManager.isPause)
+                OpenMenu();
             else
             {
                 CloseMenu();
             }
         }
-        
+
     }
-    private void CallMenu(){
-        GameManager.isPause=true;
+    private void OpenMenu()
+    { // CallMenu -> OpenMenu: CloseMenu랑 대비되게 함수 이름 바꿈
+        GameManager.isPause = true;
         Pause_UI.SetActive(true);
         Time.timeScale = 0f;
     }
-    private void CloseMenu(){
-        GameManager.isPause=false;
+    private void CloseMenu()
+    {
+        GameManager.isPause = false;
         Pause_UI.SetActive(false);
-        Time.timeScale=1f;
+        Time.timeScale = 1f;
     }
 }
