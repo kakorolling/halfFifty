18bc3064046e8a1659d24b68661633e9cd83040d
방인호 <02inho02@gmail.com>
2023-04-10 00:49:13 +0900

    제작대에서 버튼으로 제작 아이템 분류 기능 추가
    버튼으로 제작 할 수 있는 목록을 바꿀 수 있는 기능을 구현했습니다. 종류가 몇개가 나올지 몰라서 2개만 해놨어요

---
+++ Assets/Scripts/UI/CraftingTableSortButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingTableSortButton : MonoBehaviour
+{
+    public GameObject Scroll;
+    
+    public void ButtonClick()
+    {
+        GameObject[] Scrolls = GameObject.FindGameObjectsWithTag("Scroll");
+        foreach (GameObject Scroll in Scrolls)
+        {
+            Scroll.SetActive(false);
+        }
+        
+        Scroll.SetActive(true);
+       
+        
+    }
+}
