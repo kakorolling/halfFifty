192de9c846833fec676ec7799380581549684ce0
방인호 <02inho02@gmail.com>
2023-04-09 19:48:33 +0900

    DurabilityData.cs 충돌 제거
    충돌 생겨서 DurabilityData.cs에 MonoBehaviour, ISerial 새로 추가 한거 그냥 날릴께요

--- Assets/Scripts/Properties/DurabilityData.cs
+++ Assets/Scripts/Properties/DurabilityData.cs
@@ -1,15 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream:Assets/Scripts/Properties/DurabilityData.cs
 using System;
 
 [Serializable]
 public class DurabilityData
-=======
-using System.Runtime.Serialization;
-public class Durability : MonoBehaviour, ISerial
->>>>>>> Stashed changes:Assets/Scripts/Properties/Durability.cs
 {
     public int maxValue;
     public int value;
