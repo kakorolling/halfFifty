f9e24acd914d0fbac2a6bacf70331b3b7b9f3388
kakorolling <kakorolling@gmail.com>
2023-04-09 23:42:52 +0900

    이름바꿈
    properties에 있는것들 이름만 바꿈

---
+++ Assets/Scripts/Properties/SavedData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SavedData
+{
+    public Durability durability;
+    public Id id;
+    public Mesh mesh;
+}
