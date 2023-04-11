f316f46b76e1aa8412fa4daf9d01cd5dae35cfc2
kakorolling <kakorolling@gmail.com>
2023-04-12 03:06:43 +0900

    메인씬 작업 들어갔어요
    !!!!반드시 풀부터 하세요!!!!

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -5,6 +5,10 @@ using UnityEngine.Tilemaps;
 
 public class Chunk : MonoBehaviour
 {
+
+    //Chunk는 Tilemap의 일종의 관리자(Controller)
+    //같은 오브젝트에 존재하는 컴포넌트인 Tilemap의 SetTile을 통해 Tile값 변경
+
     public Vector2Int idx; //청크에 대한 인덱스 -> 배열이 아닌 좌표값
 
     public int[,] terrainLayer; // 지형 코드 레이어
@@ -19,7 +23,7 @@ public class Chunk : MonoBehaviour
     }
     public void UpdateTileMap()
     {
-        tilemap.SetTile()
+        //tilemap.SetTile()
         //이 함수가
     }
 }
