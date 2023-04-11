55022181468d43e1025ed41c21e1e25537e6620d
kakorolling <kakorolling@gmail.com>
2023-04-12 02:49:48 +0900

    update chunk
    chunk 스크립트 편집 -> 지금 크게 의미 없음

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Chunk : MonoBehaviour
 {
@@ -9,4 +10,16 @@ public class Chunk : MonoBehaviour
     public int[,] terrainLayer; // 지형 코드 레이어
     public List<GameObject>[,] objLayer; //오브젝트 레이어
     public bool[,] colliderLayer; //충돌체 감지 레이어
+
+    Tilemap tilemap;
+
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+    public void UpdateTileMap()
+    {
+        tilemap.SetTile()
+        //이 함수가
+    }
 }
