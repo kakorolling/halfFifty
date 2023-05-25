b99fb21cea96a033631578c40fa3d974ac4c817f
B4NG8ANG <02inho02@gmail.com>
2023-05-26 00:01:43 +0900

    제작대 UI 추가 작업
    1. 재료 이미지 정렬을 보기 좋게 바꿨고 재료의 수량 추가했습니다.
    2. 제작 창에 재료 이미지의 이미지를 동적으로 받아올수 있도록 작업하였습니다.

--- Assets/script/ui/CraftingTable/CraftingTableDetailImageScript.cs
+++ Assets/script/ui/CraftingTable/CraftingTableDetailImageScript.cs
@@ -7,10 +7,22 @@ using UnityEngine.UI;
 public class CraftingTableDetailImageScript : MonoBehaviour
 {
     
+    // 아이템 이름과 설명 변수
     public GameObject DetailNameTextObject;
     public GameObject DetailExpressionTextObject1;
     public GameObject DetailExpressionTextObject2;
+
+    // 아이템의 아이콘과 재료 아이템의 아이콘 변수
     public GameObject DetailIconImageObject;
+    public GameObject DetailIngredientIconImageObject1;
+    public GameObject DetailIngredientIconImageObject2;
+    public GameObject DetailIngredientIconImageObject3;
+    public GameObject DetailIngredientIconImageObject4;
+
+    //아이템 수량 변수
+
+
+
 
     public void SetText(string DetailNameText, string[] DetailExpressionText)
     {
@@ -21,10 +33,25 @@ public class CraftingTableDetailImageScript : MonoBehaviour
     }
 
     
-    public void SetImage(Image DetailIconImage)
+    public void SetImage(Image DetailIconImage, Image DetailIngredientIconImage1,
+                         Image DetailIngredientIconImage2, Image DetailIngredientIconImage3, Image DetailIngredientIconImage4)
     {
         Image detailIconImageComponent = DetailIconImageObject.GetComponent<Image>();
+        Image detailIngredientIconImageComponent1 = DetailIngredientIconImageObject1.GetComponent<Image>();
+        Image detailIngredientIconImageComponent2 = DetailIngredientIconImageObject2.GetComponent<Image>();
+        Image detailIngredientIconImageComponent3 = DetailIngredientIconImageObject3.GetComponent<Image>();
+        Image detailIngredientIconImageComponent4 = DetailIngredientIconImageObject4.GetComponent<Image>();
+
         detailIconImageComponent.sprite = DetailIconImage.sprite;
+        detailIngredientIconImageComponent1.sprite = DetailIngredientIconImage1.sprite;
+        detailIngredientIconImageComponent2.sprite = DetailIngredientIconImage2.sprite;
+        detailIngredientIconImageComponent3.sprite = DetailIngredientIconImage3.sprite;
+        detailIngredientIconImageComponent4.sprite = DetailIngredientIconImage4.sprite;
+    }
+
+    public void SetAmount()
+    {
+
     }
     
 
--- Assets/script/ui/CraftingTable/CraftingTableImageClick.cs
+++ Assets/script/ui/CraftingTable/CraftingTableImageClick.cs
@@ -13,6 +13,7 @@ public class CraftingTableImageClick : MonoBehaviour, IPointerClickHandler
     {
         {"나무 삽", new string[]{"종류: 도구", "땅을 팔 수 있는 나무 삽입니다."}},
         {"나무 칼", new string[]{"종류: 도구", "공격 할 수 있는 나무 칼입니다."}},
+        {"나무 도끼", new string[]{"종류: 도구", "벌목 할 수 있는 나무 도끼입니다."}},
         {"사과 파이", new string[]{"종류: 음식", "먹으면 체력을 회복하는 사과 파이입니다."}},
         
     };
@@ -22,9 +23,12 @@ public class CraftingTableImageClick : MonoBehaviour, IPointerClickHandler
         this.clickedObject = eventData.pointerPress;
         Debug.Log("Clicked object: " + clickedObject.name);
         Debug.Log("Clicked object: " + clickedObject.transform.GetChild(1).GetComponent<Text>().text);
+        
         CraftingTableDetailImageScript DetailScript = DetailImage.GetComponent<CraftingTableDetailImageScript>();
         DetailScript.SetText(clickedObject.transform.GetChild(1).GetComponent<Text>().text, dic[clickedObject.transform.GetChild(1).GetComponent<Text>().text]);
-        DetailScript.SetImage(clickedObject.transform.GetChild(0).GetComponent<Image>());
+        DetailScript.SetImage(clickedObject.transform.GetChild(0).GetComponent<Image>(), clickedObject.transform.GetChild(2).GetComponent<Image>(),
+                            clickedObject.transform.GetChild(3).GetComponent<Image>(), clickedObject.transform.GetChild(4).GetComponent<Image>(),
+                            clickedObject.transform.GetChild(5).GetComponent<Image>());
     }
 
     public void OnClickImage()
