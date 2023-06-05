de24b5ec7e177c7bd29e848afb2399e1d405a312
B4NG8ANG <02inho02@gmail.com>
2023-06-06 03:35:15 +0900

    시험용 버튼 수정
    시험용으로 쓸 버튼을 약간 수정하였습니다. 게임에 실제 사용되지는 않습니다.

--- Assets/script/ui/CraftingTable/Ingredienttest.cs
+++ Assets/script/ui/CraftingTable/Ingredienttest.cs
@@ -10,6 +10,11 @@ public class Ingredienttest : MonoBehaviour
         
         InventoryManager.AddItem(new Item("천", 4, "Cloth", "재료"));
         InventoryManager.AddItem(new Item("랍스터", 1, "sea_river_crayfish", "생선"));
+        InventoryManager.AddItem(new Item("나무 도막", 1, "Wood", "재료"));
+        InventoryManager.AddItem(new Item("나무 도끼", 1, "WoodAxe", "도구"));
+        InventoryManager.AddItem(new Item("나무 낚싯대", 1, "FishingRod", "도구"));
+        InventoryManager.AddItem(new Item("천", 1, "Cloth", "재료"));
+        InventoryManager.AddItem(new Item("실", 1, "Thread", "재료"));
         
 
      
